49ae710355a0aa0ec12f31982cfb4f59003a43c4
sapekshpareek <sapekshpareek136@gmail.com>
2024-05-21 06:17:48 +0530

    Response

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json({ status: 'Server is healthy' });
+    res.send('Server Running...'); 
   });
 
 app.post('/encrypt', (req, res) => {
