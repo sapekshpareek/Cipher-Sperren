c2ff0f4ff0a9e3206e56b42664e3408dd332c9e9
sarthaknagarjii <sarthaknagarjii@gmail.com>
2024-06-05 13:58:13 +0530

    update login button

--- frontend/src/components/GoogleLoginButton.js
+++ frontend/src/components/GoogleLoginButton.js
@@ -10,36 +10,46 @@ const GoogleLoginButton = () => {
   };
 
   return (
-    <Box sx={{ justifyContent: "center", alignItems: "center" , minHeight: "100vh", backgroundColor: "yellow", display: "flex"}}>
-    <GoogleOAuthProvider clientId={client}>
-      <Card
-        align="center"
-        sx={{
-          mt: 4,
-          backgroundColor: "red",
-          p: 2,
-          m: 2,
-          maxWidth: "50%",
-          margin: "0 auto",
-          justifyContent: "center",
-          alignItems: "center",
-          paddingY: 2,
-          paddingX: 6,
-          borderRadius: 2,
-        }}
-      >
-        <Box sx={{  paddingBottom: 8 , paddingTop: 6}}>
-          <h1>Login</h1>
-        </Box>
-        <Box sx={{ paddingBottom: 8 }}>
-        <GoogleLogin
-          onSuccess={handleLogin}
-          onFailure={(error) => console.error("Login failed:", error)}
-          logo_alignment="left"
-        />
-        </Box>
-      </Card>
-    </GoogleOAuthProvider>
+    <Box
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#f0d7f5",
+        objectFit: "fill",
+        display: "flex",
+      }}
+    >
+      <GoogleOAuthProvider clientId={client}>
+        <Card
+          align="center"
+          sx={{
+            mt: 4,
+            backgroundColor: "#ffffff",
+            p: 2,
+            m: 2,
+            maxWidth: "50%",
+            margin: "0 auto",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingY: 2,
+            paddingX: 6,
+            borderRadius: 2,
+          }}
+        >
+          <Box sx={{ paddingBottom: 8, paddingTop: 6 }}>
+            <h1>Login to</h1>
+            <h1>Cipher Sperren</h1>
+          </Box>
+          <Box sx={{ paddingBottom: 8 }}>
+            <GoogleLogin
+              onSuccess={handleLogin}
+              onFailure={(error) => console.error("Login failed:", error)}
+              logo_alignment="left"
+            />
+          </Box>
+        </Card>
+      </GoogleOAuthProvider>
     </Box>
   );
 };
